d0da61c7cd92fd5037a50d0c7821bad47b130c6d
Damiao Gomes <damiao28.contato@gmail.com>
2025-06-11 02:24:55 -0300

    🔨 refac(schema): removido duplicidade nas validações

--- src/schemas/category.ts
+++ src/schemas/category.ts
@@ -1,23 +1,8 @@
 import { z } from "zod";
 
-export const newCategorySchema = z.object({
+export const CategorySchema = z.object({
   title: z
     .string()
     .min(3, "Titulo deve ter pelo menos 3 caracteres")
     .regex(/^[a-zA-Z\s]+$/, "Titulo não deve conter caracteres especiais ou números"),
-})
-
-export const updateCategorySchema = z.object({
-  id: z
-    .number(),
-
-  title: z
-    .string()
-    .min(3, "Titulo deve ter pelo menos 3 caracteres")
-    .regex(/^[a-zA-Z\s]+$/, "Titulo não deve conter caracteres especiais ou números"),
-})
-
-export const deleteCategorySchema = z.object({
-  id: z
-    .number(),
 })
